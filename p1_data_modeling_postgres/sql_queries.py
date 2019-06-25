c31d060c12ea0c125aeb1b5e1bdaa5c40c8263f7
tommytracey <tommytracey@gmail.com>
2019-06-25 12:53:30 -0500

    latest updates

--- p1_data_modeling_postgres/sql_queries.py
+++ p1_data_modeling_postgres/sql_queries.py
@@ -76,7 +76,7 @@ songplay_table_insert = ("""
 user_table_insert = ("""
     INSERT INTO users (user_id, first_name, last_name, gender, level)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO UPDATE
+    ON CONFLICT DO NOTHING;
 """)
 
 song_table_insert = ("""
