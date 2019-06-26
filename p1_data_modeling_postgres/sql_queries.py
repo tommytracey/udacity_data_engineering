f8db85ab826ad97d7d384c247ecaeb4aaa47effd
tommytracey <tommytracey@gmail.com>
2019-06-26 12:16:42 -0500

    first working version

--- p1_data_modeling_postgres/sql_queries.py
+++ p1_data_modeling_postgres/sql_queries.py
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS artists (
     artist_id    VARCHAR     NOT NULL,
     name         VARCHAR     NOT NULL,
     location     TEXT,
-    lattitude    FLOAT,
+    latitude     FLOAT,
     longitude    FLOAT)
 """)
 
@@ -76,35 +76,43 @@ songplay_table_insert = ("""
 user_table_insert = ("""
     INSERT INTO users (user_id, first_name, last_name, gender, level)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING;
+    ON CONFLICT (user_id)
+    DO UPDATE
+    SET first_name=EXCLUDED.first_name, last_name=EXCLUDED.last_name, gender=EXCLUDED.gender, level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
     INSERT INTO songs (song_id, title, artist_id, year, duration)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING;
+    ON CONFLICT (song_id)
+    DO UPDATE
+    SET title=EXCLUDED.title, artist_id=EXCLUDED.artist_id, year=EXCLUDED.year, duration=EXCLUDED.duration
 """)
 
 artist_table_insert = ("""
-    INSERT INTO artists (artist_id, name, location, lattitude, longitude)
+    INSERT INTO artists (artist_id, name, location, latitude, longitude)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING;
+    ON CONFLICT (artist_id)
+    DO UPDATE
+    SET name=EXCLUDED.name, location=EXCLUDED.location, latitude=EXCLUDED.latitude, longitude=EXCLUDED.longitude
 """)
 
 time_table_insert = ("""
     INSERT INTO time (start_time, hour, day, week, month, year, weekday)
     VALUES (%s, %s, %s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING;
+    ON CONFLICT (start_time)
+    DO UPDATE
+    SET hour=EXCLUDED.hour, day=EXCLUDED.day, week=EXCLUDED.week, month=EXCLUDED.month, year=EXCLUDED.year, weekday=EXCLUDED.weekday
 """)
 
 # FIND SONGS
 
-song_select = ("""rollback;
-SELECT songs.song_id, artists.artist_id FROM songs
+song_select = ("""SELECT songs.song_id, artists.artist_id FROM songs
 JOIN artists ON songs.artist_id=artists.artist_id
 WHERE songs.title=%s
 AND artists.name=%s
-AND songs.duration=%s""")
+AND songs.duration=%s
+""")
 
 # QUERY LISTS
 
