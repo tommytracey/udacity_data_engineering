e143428f07628fc40fd1faf5ad3fb985a7c9a451
tommytracey <tommytracey@gmail.com>
2019-06-26 18:10:28 -0500

    etl.py working version

--- p1_data_modeling_postgres/etl.py
+++ p1_data_modeling_postgres/etl.py
@@ -7,57 +7,68 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
-    
+
     # insert artist record
-    artist_data = 
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df.loc[df['page'] == "NextSong"].reindex()
 
     # convert timestamp column to datetime
-    t = 
-    
+    t = pd.to_datetime(df.ts, unit='ms')
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.day_name()]
+    column_labels = ['ts', 'hour', 'day', 'week of year', 'month', 'year', 'weekday']
+    dictionary = dict(zip(column_labels, time_data))
+    time_df = pd.DataFrame.from_dict(dictionary)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
     # insert songplay records
+    log_records_checked = 0
+    songplay_rows_inserted = 0
+
     for index, row in df.iterrows():
-        
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+        log_records_checked += 1
+
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
-        cur.execute(songplay_table_insert, songplay_data)
+        if songid:
+            songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+            cur.execute(songplay_table_insert, songplay_data)
+            songplay_rows_inserted += 1
+            print('\nsongplay row inserted: \n[{}] {}\n'.format(index, songplay_data))
+
+    # verify songplay records checked vs. inserted
+    print('\n---------\ntotal log records checked in this file = {}'.format(log_records_checked))
+    print('total rows inserted = {}'.format(songplay_rows_inserted))
 
 
 def process_data(cur, conn, filepath, func):
